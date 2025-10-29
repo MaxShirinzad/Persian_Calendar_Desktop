2a9299395b28f4f44287ae7e20eca48c4812cf79
max.shirinzad@gmail.com <max.shirinzad@gmail.com>
2025-10-30 01:29:18 +0330

    Change window size in Electron desktop view

--- main.js
+++ main.js
@@ -3,13 +3,18 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 900,
-    height: 700,
+    width: 1200,  // عرض پنجره
+    height: 800,  // ارتفاع پنجره
+    minWidth: 1000,  // حداقل عرض
+    minHeight: 700,  // حداقل ارتفاع
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false
-    }
+    },
+    center: true,  // نمایش پنجره در وسط صفحه
+    show: true,   // ابتدا پنجره نشان داده نشود تا زمانی که آماده شود
+    // icon: path.join(__dirname, 'assets/icon.png') // آیکون برنامه (اختیاری)
   });
 
   //win.loadURL('http://localhost:5173'); // Vite dev server
